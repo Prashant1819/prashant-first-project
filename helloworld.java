2255d9ffcd6067a8c3b46ccb80c253443ad77663
prash <prash.1819.m@gmail.com>
2022-06-28 22:32:08 +0530

    new project code added

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class helloworld {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
---
+++ prashant.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class prashant {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
